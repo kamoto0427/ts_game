25288e541a8305944697af5df64025d7c0ef577e
kamoto0427 <takahiro.08039@gmail.com>
2021-12-08 23:08:54 +0900

    バリデーションの実装 ・受け取る文字列の数がanswer.length個である ・それぞれの文字列が、answerSourceに含まれるいずれかの文字列である ・それぞれの文字列に重複がない

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,12 @@ class HitAndBlow {
 
   async play() {
     const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+
+    if (!this.validate(inputArr)) {
+      printLine('無効な値です')
+      await this.play()
+      return
+    }
     const result = this.check(inputArr)
 
     if (result.hit !== this.answer.length) {
@@ -62,6 +68,14 @@ class HitAndBlow {
   end() {
     printLine(`正解です!\n試行回数: ${this.tryCount}回`)
     process.exit()
+  }
+
+  private validate(inputArr: string[]) {
+    const isLengthValid = inputArr.length === this.answer.length
+    const isAllAnswerSourceOption = inputArr.every((val) => this.answerSource.includes(val))
+    const isAllDifferentValues = inputArr.every((val, i) => inputArr.indexOf(val) === i)
+
+    return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues
   }
 }
 
